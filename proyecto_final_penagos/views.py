823363f89e3151df3e24c2d6fff959d0dd326714
jorgepenagos23 <jorgepenagos50@gmail.com>
2023-06-01 03:52:23 -0600

    PROYECTO SERVICIOS WEB PENAGOS

--- proyecto_final_penagos/views.py
+++ proyecto_final_penagos/views.py
@@ -1,57 +1,69 @@
 from django.shortcuts import render
-from penagos_app.models import Palabra
+from penagos_app.models import Palabra, ResidentEvil, Fortnite, Videojuegos
 from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from penagos_app.models import Fortnite, ResidentEvil
 import nltk
 
-
-# Create your views here.
 nltk.download('punkt')  # Descarga los recursos necesarios para el tokenizador de NLTK
 
 def chatbot_view(request):
     if request.method == 'POST':
         user_input = request.POST.get('user_input', '')
-        
+
         try:
             palabra_aprendida = Palabra.objects.get(palabra=user_input.lower())
             respuesta = palabra_aprendida.respuesta
 
         except Palabra.DoesNotExist:
-            if user_input.lower() == 'fortnite':
-                respuesta = "Fortnite es un juego popular lanzado en 2018."
+            respuesta2 = "desconozco lo que preguntaste"
+            tokens = word_tokenize(user_input)
 
-            elif user_input.lower() == 'cuándo salió fortnite':
-                respuesta = "Fortnite fue lanzado el 25 de julio de 2017."
+            # Buscar coincidencias en la tabla Fortnite
+            coincidencias_fortnite = Fortnite.objects.filter(nombre__icontains=user_input.lower())
+
+            # Buscar coincidencias en la tabla ResidentEvil
+            coincidencias_resident_evil = ResidentEvil.objects.filter(nombre__icontains=user_input.lower())
+
+            # Buscar coincidencias en la tabla Videojuegos
+            coincidencias_videojuegos = Videojuegos.objects.filter(nombre__icontains=user_input.lower())
+
+            if coincidencias_fortnite:
+                respuesta = f"¡Encontré coincidencias en '{user_input}' para tu pregunta! Aquí hay algunos resultados:\n"
+                for coincidencia in coincidencias_fortnite:
+                    respuesta += f"Nombre: {coincidencia.nombre}\n"
+                    respuesta += f"Descripción: {coincidencia.descripcion}\n\n"
+
+            elif coincidencias_resident_evil:
+                respuesta = f"¡Encontré coincidencias en '{user_input}' para tu pregunta! Aquí hay algunos resultados:\n"
+                for coincidencia in coincidencias_resident_evil:
+                    respuesta += f"Nombre: {coincidencia.nombre}\n"
+                    respuesta += f"Género: {coincidencia.genero}\n"
+                    respuesta += f"Plataforma: {coincidencia.plataforma}\n"
+                    respuesta += f"Lanzamiento: {coincidencia.lanzamiento}\n"
+                    respuesta += f"Descripción: {coincidencia.descripcion}\n\n"
+
+            elif coincidencias_videojuegos:
+                respuesta = f"¡Encontré coincidencias en '{user_input}' para tu pregunta! Aquí hay algunos resultados:\n"
+                for coincidencia in coincidencias_videojuegos:
+                    respuesta += f"Nombre: {coincidencia.nombre}\n"
+                    respuesta += f"Género: {coincidencia.genero}\n"
+                    respuesta += f"Plataforma: {coincidencia.plataforma}\n"
+                    respuesta += f"Lanzamiento: {coincidencia.lanzamiento}\n"
+                    respuesta += f"Descripción: {coincidencia.descripcion}\n\n"
 
             else:
-                respuesta = "Lo siento, no puedo responder esa pregunta."
+                # No se encontraron coincidencias en las tablas de videojuegos
+                respuesta = respuesta2
 
-            palabra = Palabra(palabra=user_input.lower(), respuesta=respuesta)
-            palabra.save()
+                # Obtener sugerencias de palabras en la tabla Palabra
+                sugerencias = Palabra.objects.filter(palabra__icontains=user_input.lower()).values_list('palabra', flat=True)
+                if sugerencias:
+                    respuesta += "¿Quisiste decir alguna de estas palabras?\n"
+                    respuesta += "\n".join(sugerencias)
 
-        nltk.download('punkt')  # Descarga los recursos necesarios para la tokenización
-
-        tokens = word_tokenize(user_input)  # Tokeniza la pregunta en palabras individuales
-        print(tokens)
-
-        if user_input.lower() == 'fortnite':
-            try:
-                fortnite = Fortnite.objects.get(nombre='Fortnite')
-                respuesta = f"Fortnite es un juego popular con las siguientes características: {fortnite.descripcion}"
-            except Fortnite.DoesNotExist:
-                respuesta = "No encontré información sobre Fortnite en la base de datos."
-
-        elif user_input.lower() == 'resident evil':
-            try:
-                resident_evil = ResidentEvil.objects.get(nombre='Resident Evil')
-                respuesta = f"Resident Evil es una serie de juegos de terror con las siguientes características: {resident_evil.descripcion}. Fecha de lanzamiento: {resident_evil.lanzamiento}, Plataforma: {resident_evil.plataforma}, Género: {resident_evil.genero}"
-            except ResidentEvil.DoesNotExist:
-                respuesta = "No encontré información sobre Resident Evil en la base de datos."
-
-        else:
-            respuesta = "Lo siento, no puedo responder esa pregunta."
+                # Guardar la palabra ingresada en la tabla Palabra
+                palabra = Palabra(palabra=tokens, respuesta=respuesta2)
+                palabra.save()
 
         return render(request, 'chatbot.html', {'respuesta': respuesta})
-    
+
     return render(request, 'chatbot.html')
